e4c4067d1e84237ffd092129a682d78519d3318f
Damir <absalyamov05@list.ru>
2024-09-10 03:53:47 +0300

    Задание 1

---
+++ Lab1LOAIZ/Lab1LOAIZ/Lab1LOAIZ.cpp
@@ -0,0 +1,54 @@
+﻿//Задание 1
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	const int SIZE = 10;
+
+	int Arr[SIZE];
+	int Max = INT_MIN;
+	int Min = INT_MAX;
+	int Razn;
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		Arr[i] = i;
+	}
+
+	cout << "Массив: ";
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << Arr[i] << " ";
+	}
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		if (Max < Arr[i])
+		{
+			Max = Arr[i];
+		}
+	}
+
+	cout << "\nМаксимальный элемент: " << Max;
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		if (Min > Arr[i])
+		{
+			Min = Arr[i];
+		}
+	}
+
+	cout << "\nМинимальный элемент: " << Min;
+
+	Razn = Max - Min;
+
+	cout << "\nРазница между максимальным и минимальным элементом: " << Razn;
+
+	return 0;
+}
