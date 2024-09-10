dda25ae3b2521a2b968fe13c266a049e1c8e68cc
Damir <absalyamov05@list.ru>
2024-09-10 14:22:12 +0300

    Доп задание 5

--- Lab1LOAIZ/Lab1LOAIZ/Lab1LOAIZ.cpp
+++ Lab1LOAIZ/Lab1LOAIZ/Lab1LOAIZ.cpp
@@ -8,15 +8,22 @@ struct Student
 {
 	char lastName[30];
 	char firstName[30];
+	char zachetka[30];
 };
 
-void searchStudent(struct Student students[], int N, char lastName[], char firstName[])
+void searchStudent(struct Student students[], int N, char lastName[], char firstName[], char zachetka[])
 {
+	int flag = 0;
 	for (int i = 0; i < N; i++) {
-		if (strcmp(students[i].lastName, lastName) == 0 && strcmp(students[i].firstName, firstName) == 0)
+		if (strcmp(students[i].lastName, lastName) == 0 && strcmp(students[i].firstName, firstName) == 0 && strcmp(students[i].zachetka, zachetka) == 0)
 		{
-			printf("Найдено: %s %s\n", students[i].lastName, students[i].firstName);
+			printf("Найдено: %s %s %s\n", students[i].lastName, students[i].firstName, students[i].zachetka);
+			flag = 1;
 		}
+	}
+	if (flag == 0)
+	{
+		printf("Такого студента нет");
 	}
 }
 
@@ -25,15 +32,16 @@ int main()
 	setlocale(LC_ALL, "ru");
 	struct Student students[] =
 	{
-	{"Иванов", "Иван"},
-	{"Петров", "Петр"},
-	{"Сидоров", "Сергей"},
+	{"Иванов", "Иван", "12345"},
+	{"Петров", "Петр", "22222"},
+	{"Сидоров", "Сергей", "01010"},
 	};
 
 	char lastName[] = "Петров";
 	char firstName[] = "Петр";
+	char zachetka[] = "2222";
 
-	searchStudent(students, 3, lastName, firstName);
+	searchStudent(students, 3, lastName, firstName, zachetka);
 
 	return 0;
 }
